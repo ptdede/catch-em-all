2e08f54c03c9ea8e3a61c1f83ec5a2dca4dd938d
ptdede <putu.dede@gmail.com>
2021-03-11 08:58:18 +0800

    Init pokemon list using graphql

--- containers/PokemonList/PokemonList.tsx
+++ containers/PokemonList/PokemonList.tsx
@@ -1,5 +1,29 @@
+import { usePokemonList } from './PokemonList.state';
+
 export const PokemonList = () => {
+  const {
+    data,
+    error,
+    loading,
+    loadingMorePokemon,
+    loadMorePokemon
+  } = usePokemonList()
+
+  if (error) return <p>error</p>
+
+  if (loading && !loadingMorePokemon) return <div>Loading</div>
+
+  const { pokemons } = data
+
   return (
-    <h1>Pokemon List</h1>
+    <div>
+      <ol>
+        {
+          pokemons.results.map(res => <li key={`pokemon-${res.id}`}>{res.name}</li>)
+        }
+      </ol>
+      
+      <button onClick={loadMorePokemon}>fetch more</button>
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-const Home = () => {
-  return (
-    <h1>CATCH EM ALL</h1>
-  )
+import { PokemonList } from '../containers';
+import { pokemonsQueryVariables, POKEMONS_GQL } from '../graphql/pokemons.gql';
+import { initializeApollo } from '../libs/apolloClient';
+
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  await apolloClient.query({
+    query: POKEMONS_GQL,
+    variables: pokemonsQueryVariables,
+  })
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    revalidate: 1,
+  }
 }
 
-export default Home
+export default PokemonList
