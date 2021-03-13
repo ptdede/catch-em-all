045fd78276a6ade731ad929fa356beca3fd35b39
ptdede <putu.dede@gmail.com>
2021-03-13 13:56:02 +0800

    pokemon detail desktop design

--- containers/PokemonDetail/PokemonDetail.styled.ts
+++ containers/PokemonDetail/PokemonDetail.styled.ts
@@ -1,9 +1,19 @@
 import styled from '@emotion/styled';
 
 const Wrapper = styled.div`
+
+  @media screen and (min-width: 600px) {
+    display: flex;
+  }
 `
 
-const DetailWrapper = styled.div<{ color: string }>`
+const DetailWrapper = styled.div`
+  @media screen and (min-width: 600px) {
+    margin-right: 2rem;
+  }
+`
+
+const DetailContent = styled.div<{ color: string }>`
   position: relative;
   display: flex;
   flex-direction: column;
@@ -20,9 +30,19 @@ const DetailWrapper = styled.div<{ color: string }>`
     border-radius: 50%;
     background-color: ${p => p.color ?? "#ececec"};
     transform: translateX(-50%);
-    z-index: -1;
+    z-index: 0;
     transition: background-color 600ms ease-in-out;
     transition-delay: 600ms;
+  }
+
+  @media screen and (min-width: 600px) {
+    padding: 2rem;
+    border: solid 2px #ececec;
+    border-radius: 6px;
+
+    &:before {
+      top: 2rem;
+    }
   }
 `
 
@@ -30,19 +50,27 @@ const AttributesWrapper = styled.div`
   margin-top: 2rem;
   padding: 2rem;
   margin-bottom: -6rem;
-  /* background-color: rgba(0,58,112, 1); */
   background-color: #ececec;
+
+  @media screen and (min-width: 600px) {
+    margin-top: 0;
+    margin-bottom: 0;
+    border-radius: 6px;
+  }
+`
+
+const AttributesTitle = styled.h4`
+  font-weight: 700;
+  font-size: 1.2em;
+  border-bottom: solid 1px rgba(111, 111, 111, .3);
+  padding-bottom: .5rem;
 `
 
 const Image = styled.img`
   width: 250px;
   height: 250px;
   object-fit: contain;
-
-  @media screen and (min-width: 600px) {
-    width: 300px;
-    height: 300px;
-  }
+  z-index: 1;
 `
 
 const Name = styled.h1`
@@ -71,13 +99,25 @@ const Move = styled.p<{ color: string }>`
   padding: .5rem 1rem;
   margin: .2rem .3rem;
   border-radius: 1000px;
-  background-color: ${p => p.color ?? "rgba(0,58,112, 1)"};
+  background-color: ${p => p.color ?? "#ececec"};
+  transition: background-color 300ms ease-in-out;
 `
 
 const MoveWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin-top: 1rem;
+`
+
+const LoadingWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export const TPokemonDetail = {
@@ -89,5 +129,8 @@ export const TPokemonDetail = {
   TypeWrapper,
   MoveWrapper,
   DetailWrapper,
+  DetailContent,
+  LoadingWrapper,
+  AttributesTitle,
   AttributesWrapper,
 }
--- containers/PokemonDetail/PokemonDetail.tsx
+++ containers/PokemonDetail/PokemonDetail.tsx
@@ -5,47 +5,59 @@ import { usePokemonDetail } from './PokemonDetail.state'
 import { TPokemonDetail } from './PokemonDetail.styled'
 
 export const PokemonDetail = () => {
-  const router = useRouter()
-  const { name } = router.query as { name: string }
-  const { data, error, colors, loading } = usePokemonDetail(name)
-  
-  if (loading || !name) return <Loading />
+  const { query, isReady } = useRouter()
 
-  if (error) return <p>error</p>
+  const name = query.name
+
+  const { data, error, colors, loading } = usePokemonDetail(name as string)
+
+  if (loading || (!name && !isReady)) {
+    return (
+      <TPokemonDetail.LoadingWrapper>
+        <Loading />
+      </TPokemonDetail.LoadingWrapper>
+    )
+  }
+
+  if (error || (isReady && !name)) return <p>error</p>
 
   const { pokemon } = data
 
   return (
     <TPokemonDetail.Wrapper>
-      <TPokemonDetail.DetailWrapper
-        color={colors.dominant}
-      >
-        <TPokemonDetail.Image
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name}
-        />
-        <TPokemonDetail.Name>
-          {pokemon.name}
-        </TPokemonDetail.Name>
-        <TPokemonDetail.TypeWrapper>
-          {
-            pokemon.types.map((value, index) => {
-              const type = value.type.name
+      <TPokemonDetail.DetailWrapper>
+        <TPokemonDetail.DetailContent
+          color={colors.dominant}
+        >
+          <TPokemonDetail.Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
+          <TPokemonDetail.Name>
+            {pokemon.name}
+          </TPokemonDetail.Name>
+          <TPokemonDetail.TypeWrapper>
+            {
+              pokemon.types.map((value, index) => {
+                const type = value.type.name
 
-              return (
-                <TPokemonDetail.Type
-                  key={`type-${index}`}
-                  color={POKEMONS_TYPES[type].colorDark}
-                >
-                  {type}
-                </TPokemonDetail.Type>
-              )
-            })
-          }
-        </TPokemonDetail.TypeWrapper>
+                return (
+                  <TPokemonDetail.Type
+                    key={`type-${index}`}
+                    color={POKEMONS_TYPES[type].colorDark}
+                  >
+                    {type}
+                  </TPokemonDetail.Type>
+                )
+              })
+            }
+          </TPokemonDetail.TypeWrapper>
+        </TPokemonDetail.DetailContent>
       </TPokemonDetail.DetailWrapper>
       <TPokemonDetail.AttributesWrapper>
-        <h4>MOVES</h4>
+        <TPokemonDetail.AttributesTitle>
+          MOVES
+        </TPokemonDetail.AttributesTitle>
 
         <TPokemonDetail.MoveWrapper>
           {
