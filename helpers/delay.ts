864d23354492f002364166dcec04b7a99062d4cc
ptdede <putu.dede@gmail.com>
2021-03-14 23:09:57 +0800

    add delay helpers

---
+++ helpers/delay.ts
@@ -0,0 +1,8 @@
+export const delay = (timeout = 5000) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      resolve(true)
+    }, timeout);
+  })
+}
