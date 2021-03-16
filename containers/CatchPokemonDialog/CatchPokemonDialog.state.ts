62ad44b138fe54c1514f39ef02bb06cb91d5b430
Dede Adhyatmika <putu.dede@gmail.com>
2021-03-16 10:16:12 +0800

    [Hotfix] My Pokemon not updated after caught and update Pokemon name (#6)

--- containers/CatchPokemonDialog/CatchPokemonDialog.state.ts
+++ containers/CatchPokemonDialog/CatchPokemonDialog.state.ts
@@ -2,11 +2,14 @@ import { useRouter } from 'next/router'
 import { IPokemon } from '@graphql/pokemon.gql'
 import { useUpdateMyPokemon } from '@database/my-pokemon'
 import { useCatchPokemon } from '@providers/CatchPokemon/CatchPokemon.provider'
+import { useMyPokemonData } from '@providers/MyPokemonData/MyPokemonData.provider'
 
 export const useCatchPokemonDialog = () => {
   const { push } = useRouter()
   const { closeCatchWindow } = useCatchPokemon()
   const { updateMyPokemon, loading: updateLoading, error: updateError } = useUpdateMyPokemon()
+
+  const { pokemons: myPokemon, setPokemonData: setMyPokemonData } = useMyPokemonData()
 
   const updatePokemonName = async (name: string, pokemon: IPokemon) => {
     try {
@@ -18,10 +21,21 @@ export const useCatchPokemonDialog = () => {
         return
       }
 
-      await updateMyPokemon({
+      const updatePokemon = {
         ...pokemon,
         ownedName: name
+      }
+
+      await updateMyPokemon(updatePokemon)
+
+      const filtered = myPokemon.map(poke => {
+        if (poke.ownedName === pokemon.ownedName) {
+          return updatePokemon
+        }
+        return  poke
       })
+
+      setMyPokemonData(filtered)
       
       push(redirect)
       closeCatchWindow()
